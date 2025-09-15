67f893e0b46179f8bc18bae845bcddad591f79a7
apcarreno9 <diegoalejandro.peralta@bbva.com>
2025-09-15 15:38:32 +0000

    Cambio de titulo de prueba

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My Streamlit new app")
+st.title("ESTO SE DEBERIA VER BONITO")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
